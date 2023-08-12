35c67a4123c73d23f6c5149da9e3d542fb545f97
Krithik <krithiksehgal@gmail.com>
2023-08-12 00:34:41 -0700

    added deleteUser route

--- api/src/controllers/authController.ts
+++ api/src/controllers/authController.ts
@@ -57,3 +57,16 @@ export const registerUser = async (req: Request, res: Response) => {
       res.status(500).json({ error: 'Failed to login' });
     }
   };
+
+    export const deleteUser = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        try {
+            const deletedUser = await UserSchema.findByIdAndDelete(id);
+            if (!deletedUser) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            res.status(200).json(deletedUser);
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to delete user' });
+        }
+    };
--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/authController';
+import { loginUser, registerUser, deleteUser } from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.delete('/delete/:id', deleteUser);
 
 export default router;
